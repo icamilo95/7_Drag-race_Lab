0fbf384a59b118c4b3d109d9b0e024eb2a344af9
Camilo Ordonez <icoj@gmail.com>
2015-01-25 23:00:24 -0800

    Includes crowd and half reset

--- game.js
+++ game.js
@@ -1,18 +1,15 @@
 function Game(){
 	dragster(); 
 	turnon();
-	
-	
 }
 
 function moves() {
-	window.addEventListener("keydown",function(event){
+	window.addEventListener("keyup",function(event){
 		if (event.keyCode === 39) {
 			advance();					
 		}	
 	});
 }
-
 
 	// function preStageLights (){
 	// for (var i = 0; i < 8; i++) {
@@ -36,14 +33,12 @@ function moves() {
 	// }
 
 
-
 	function preStageLights (){
 	var bulb1 = document.getElementById("start1");	
 	bulb1.setAttribute("src", "img/start1-off.png");
 	var bulb2 = document.getElementById("start2");
 	bulb2.setAttribute("src", "img/start2-on.png");	
 	setTimeout(yellowLight1,1500);
-	
 	}
 	
 	function yellowLight1(){
@@ -71,25 +66,17 @@ function moves() {
 	function greenLight(){
 	var bulb6 = document.getElementById("start6");
 	bulb6.setAttribute("src", "img/start6_on.png");
-	
 	var bublb5 = document.getElementById("start5");
 	bublb5.setAttribute("src", "img/start5_off.png");
-	
-
-
 	}	
 
 	function redLight () {
-		var bulb7 = document.getElementById("start7");
-		bulb7.setAttribute("src", "img/start7_on.png");
-
-		var bublb6 = document.getElementById("start6");
-		bublb6.setAttribute("src", "img/start6_off.png");
-		setTimeout(alert("Foul!"),5000);
+	var bulb7 = document.getElementById("start7");
+	bulb7.setAttribute("src", "img/start7_on.png");
+	var bublb6 = document.getElementById("start6");
+	bublb6.setAttribute("src", "img/start6_off.png");
+	setTimeout(alert("Foul!"),5000);
 	}
-
-
-
 
 function dragster(){
 	this.$carrito = document.getElementById("dragster1");
@@ -104,9 +91,7 @@ function turnon(){
 			moves();
 		}
 	});	
-
 }
-
 
 function advance () {
 	this.$carrito.style.left = parseInt(this.$carrito.style.left,10) + 10 + "px";
@@ -121,21 +106,26 @@ function advance () {
 		console.log("Inicia");
 	}else if (this.$carrito.style.left === "936px") {
 		 t2 = performance.now();
-		var finalTime = t2 - t1; 	
-		 
-		finalTime2 = (finalTime / 1000) % 60;
-		console.log(finalTime2);
+		var finalTime = (t2 - t1); 
+		finalTime2 = ((finalTime/1000) % 60)/2.1;
+		var totalTime = document.getElementById("watch-1").innerHTML = "00:00:" + (finalTime2);
+		alert("Finished. Your time is: " + totalTime + " seconds");
+	}
+}
 
-		var totalTime = document.getElementById("watch-1").innerHTML = "00:00:00:" + Math.round(finalTime2);
-
-		alert("Finished. Your time is:" + totalTime + " milliseconds");
+	function resetGame(){
+	document.getElementById("start1").setAttribute("src", "img/start1-on.png");
+	document.getElementById("start2").setAttribute("src", "img/start2-off.png");	
+	document.getElementById("start3").setAttribute("src", "img/start3_off.png");
+	document.getElementById("start4").setAttribute("src", "img/start4_off.png");		
+	document.getElementById("start5").setAttribute("src", "img/start5_off.png");	
+	document.getElementById("start6").setAttribute("src", "img/start6_off.png");	
+	document.getElementById("start7").setAttribute("src", "img/start7_off.png");
+	dragster();	
+	document.getElementById("OffMessage").innerHTML = ("The car is Off");
+	document.getElementById("OffMessage").setAttribute("class","list-group-item list-group-item-danger");	
 	}
 
-	
-
-
-
-}
 
 
 Game();
